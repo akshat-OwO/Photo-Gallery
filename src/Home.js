7691f4091633e526f0b77220190f78c1972f76d2
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-28 10:18:29 +0530

    outputting list

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 
 const Home = () => {
-    // let name = 'mario'
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    
-    const handleClick = () =>{
-        setName('luigi');
-        setAge(30);
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'Gray and Brown Mountains', img: require('./images/mountain1.jpg'), author: 'mario', id: 1},
+        {title: 'Mountain Covered in Snow', img: require('./images/mountain2.jpg'), author: 'yoshi', id: 2},
+        {title: 'Snow Top Mountains', img: require('./images/mountain3.jpg'), author: 'mario', id: 3},
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={ handleClick }>Click Me</button>
+            { blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <img src={ blog.img } alt="" srcset="" />
+                </div>
+            )) }
         </div>
      );
 }
