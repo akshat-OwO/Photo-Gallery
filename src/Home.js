57b0a0d51b1a9b6c3746a94422258d88af2dfef9
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-28 21:55:36 +0530

    handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,24 @@ import ImageList from './ImageList';
 const Home = () => {
     const [images, setImages] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() =>{
         setTimeout(() =>{
             fetch('http://localhost:8000/images')
             .then(res =>{
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource');
+                }
                 return res.json();
             })
             .then((data) =>{
                 setImages(data);
+                setLoading(false);
+                setError(null);
+            })
+            .catch((err) =>{
+                setError(err.message);
                 setLoading(false);
             });
         }, 1000);
@@ -20,6 +29,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
             {loading && <div>Loading...</div>}
             {images && <ImageList images={images} title={'Mountains'}/>}
         </div>
