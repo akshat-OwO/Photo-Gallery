f9a7cc2ebc397f68cd6860dc061c6a1a8054cd04
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-28 10:48:05 +0530

    basic styles

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
+    const [images, setImages] = useState([
         {title: 'Gray and Brown Mountains', img: require('./images/mountain1.jpg'), author: 'mario', id: 1},
         {title: 'Mountain Covered in Snow', img: require('./images/mountain2.jpg'), author: 'yoshi', id: 2},
         {title: 'Snow Top Mountains', img: require('./images/mountain3.jpg'), author: 'mario', id: 3},
+        {title: 'Black and Teal Mountains', img: require('./images/mountain4.jpg'), author: 'mario', id: 4},
     ])
 
     return ( 
         <div className="home">
-            { blogs.map((blog) =>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <img src={ blog.img } alt="" srcset="" />
+            { images.map((image) =>(
+                <div className="image-preview" key={image.id}>
+                    <h2>{ image.title }</h2>
+                    <img src={ image.img } alt="" srcset="" />
                 </div>
             )) }
         </div>
