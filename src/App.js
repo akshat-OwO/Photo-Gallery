432acff4c6fa704f299ff94a857411f0f05ce04c
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-28 08:14:16 +0530

    Dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 function App() {
+  const title = 'welcome to the new blog';
+  const likes = 10;
+  const link = 'https://www.google.com';
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked: { likes } times</p>
+        <p>{ 50 }</p>
+        <p>{ "hello world" }</p>
+        <a href= { link }>Google Site</a>
       </div>
     </div>
   );
