1a0cbd468a43bd58644a092ca65bb16e2f89663c
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-28 20:35:34 +0530

    conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,24 @@ import ImageList from './ImageList';
 
 const Home = () => {
     const [images, setImages] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() =>{
-        fetch('http://localhost:8000/images')
+        setTimeout(() =>{
+            fetch('http://localhost:8000/images')
             .then(res =>{
                 return res.json();
             })
             .then((data) =>{
                 setImages(data);
+                setLoading(false);
             });
+        }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+            {loading && <div>Loading...</div>}
             {images && <ImageList images={images} title={'Mountains'}/>}
         </div>
      );
